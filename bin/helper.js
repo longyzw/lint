9d602176199bcbb10f0cf1f7f5132f6fda86e32f
Kimi <124864508+yangls1995@users.noreply.github.com>
2023-05-18 18:50:45 +0800

    fix: 调整规则

--- bin/helper.js
+++ bin/helper.js
@@ -1,106 +1,95 @@
-const path = require("path");
-const { execSync, exec } = require("child_process");
+const path = require('path')
+const { execSync, exec } = require('child_process')
 const {
   getProcessDir,
   getFileListInDir,
   filterReg,
   getPackageJsonPath,
   writeFileSync,
-  getAbsolutePath,
-} = require("./utils");
+  getAbsolutePath
+} = require('./utils')
 const {
   ESLINT_PRESETS,
   STYLELINT_PRESETS,
   COMMITLINT_PRESETS,
   PRETTIER_PRESETS,
   LINT_DEPENDENCIES,
-  ALL_LINT_DEPENDENCIES_REGEXP,
-} = require("./constant");
+  ALL_LINT_DEPENDENCIES_REGEXP
+} = require('./constant')
 
 /**
  * 检查node环境 >= 16
  */
 const checkNodeEnv = () => {
-  const version = execSync("node -v");
-  const versionNum = version.toString().split(".")[0].split("v")[1];
-  console.log("nodeVersoin:", versionNum);
+  const version = execSync('node -v')
+  const versionNum = version.toString().split('.')[0].split('v')[1]
+  console.log('nodeVersoin:', versionNum)
   if (versionNum < 16) {
-    console.log(chalk.red("node版本过低，请升级至16以上"));
-    return false;
+    console.log(chalk.red('node版本过低，请升级至16以上'))
+    return false
   }
-  return true;
-};
+  return true
+}
 
 /**
  * 检查lint环境是否已经安装
  * return { installedLintName, installedLintConfigList }
  */
 const checkLintEnv = () => {
-  const processDir = getProcessDir();
-  const fileList = getFileListInDir(processDir);
-  const eslintConfigs = filterReg(fileList, ESLINT_PRESETS.configRegexp);
-  const stylelintConfigs = filterReg(fileList, STYLELINT_PRESETS.configRegexp);
-  const commitlintConfigs = filterReg(
-    fileList,
-    COMMITLINT_PRESETS.configRegexp
-  );
-  const prettierConfigs = filterReg(fileList, PRETTIER_PRESETS.configRegexp);
+  const processDir = getProcessDir()
+  const fileList = getFileListInDir(processDir)
+  const eslintConfigs = filterReg(fileList, ESLINT_PRESETS.configRegexp)
+  const stylelintConfigs = filterReg(fileList, STYLELINT_PRESETS.configRegexp)
+  const commitlintConfigs = filterReg(fileList, COMMITLINT_PRESETS.configRegexp)
+  const prettierConfigs = filterReg(fileList, PRETTIER_PRESETS.configRegexp)
 
-  const installedLintName = []; // 已经安装的lint工具名
-  const installedLintConfigList = []; // 已经安装的lint工具配置文件列表
+  const installedLintName = [] // 已经安装的lint工具名
+  const installedLintConfigList = [] // 已经安装的lint工具配置文件列表
   if (eslintConfigs.length > 0) {
-    installedLintName.push("ESLint");
-    installedLintConfigList.push(...eslintConfigs);
+    installedLintName.push('ESLint')
+    installedLintConfigList.push(...eslintConfigs)
   }
   if (stylelintConfigs.length > 0) {
-    installedLintName.push("StyleLint");
-    installedLintConfigList.push(...stylelintConfigs);
+    installedLintName.push('StyleLint')
+    installedLintConfigList.push(...stylelintConfigs)
   }
   if (commitlintConfigs.length > 0) {
-    installedLintName.push("CommitLint");
-    installedLintConfigList.push(...commitlintConfigs);
+    installedLintName.push('CommitLint')
+    installedLintConfigList.push(...commitlintConfigs)
   }
   if (prettierConfigs.length > 0) {
-    installedLintName.push("Prettier");
-    installedLintConfigList.push(...prettierConfigs);
+    installedLintName.push('Prettier')
+    installedLintConfigList.push(...prettierConfigs)
   }
 
   return {
     installedLintName,
-    installedLintConfigList,
-  };
-};
+    installedLintConfigList
+  }
+}
 
 /**
  * 移除package.json文件中dependencies中的lint相关依赖
  */
 const removeLintDependencies = () => {
-  const packagePath = getPackageJsonPath();
-  writePackageJson(packagePath, (packageJson) => {
-    const {
-      dependencies = {},
-      devDependencies = {},
-      optionalDependencies = {},
-      scripts = {},
-    } = packageJson;
+  const packagePath = getPackageJsonPath()
+  writePackageJson(packagePath, packageJson => {
+    const { dependencies = {}, devDependencies = {}, optionalDependencies = {}, scripts = {} } = packageJson
     const dependenciesList = [
       ...Object.keys(dependencies),
       ...Object.keys(devDependencies),
-      ...Object.keys(optionalDependencies),
-    ];
-    const lintDependencies = filterReg(
-      dependenciesList,
-      ALL_LINT_DEPENDENCIES_REGEXP
-    );
-    lintDependencies.forEach((item) => {
-      delete dependencies[item];
-      delete devDependencies[item];
-      delete optionalDependencies[item];
-    });
-    scripts["prepare"] && delete scripts["prepare"];
-    return packageJson;
-  });
-};
+      ...Object.keys(optionalDependencies)
+    ]
+    const lintDependencies = filterReg(dependenciesList, ALL_LINT_DEPENDENCIES_REGEXP)
+    lintDependencies.forEach(item => {
+      delete dependencies[item]
+      delete devDependencies[item]
+      delete optionalDependencies[item]
+    })
+    scripts['prepare'] && delete scripts['prepare']
+    return packageJson
+  })
+}
 
 /**
  * 写入package.json文件
@@ -108,258 +97,251 @@ const removeLintDependencies = () => {
  * @param {function} callback
  */
 const writePackageJson = (packagePath, callback) => {
-  const packageJson = require(packagePath);
-  const newPackageJson = callback(packageJson);
-  writeFileSync(packagePath, JSON.stringify(newPackageJson, null, 2));
-};
+  const packageJson = require(packagePath)
+  const newPackageJson = callback(packageJson)
+  writeFileSync(packagePath, JSON.stringify(newPackageJson, null, 2))
+}
 
 /**
  * 根据目标环境安装lint相关依赖
  */
 const installLintDependencies = async () => {
   // 根据lint要素得到lint的所有依赖
-  const lintDependencies = LINT_DEPENDENCIES;
+  const lintDependencies = LINT_DEPENDENCIES
 
   // 检查 husky、lint-staged 是否已经安装
-  const [isInstalledHusky, isInstalledLintStaged] = checkInstalledPackage([
-    "husky",
-    "lint-staged",
-  ]);
-  !isInstalledHusky && lintDependencies.push("husky");
-  !isInstalledLintStaged && lintDependencies.push("lint-staged");
+  const [isInstalledHusky, isInstalledLintStaged] = checkInstalledPackage(['husky', 'lint-staged'])
+  !isInstalledHusky && lintDependencies.push('husky')
+  !isInstalledLintStaged && lintDependencies.push('lint-staged')
 
   // 根据dependencies获取最新版本号
-  const latestLintDependencies = await getLatestLintDependencies(
-    lintDependencies
-  );
+  const latestLintDependencies = await getLatestLintDependencies(lintDependencies)
   // 将lint依赖写入package.json
-  addLintDependencies(latestLintDependencies);
+  addLintDependencies(latestLintDependencies)
 
   // 检查删除prepare脚本
-  const packageJsonPath = getPackageJsonPath();
-  const { scripts = {} } = require(packageJsonPath);
-  scripts.prepare && execSync("npm pkg delete scripts.prepare");
+  const packageJsonPath = getPackageJsonPath()
+  const { scripts = {} } = require(packageJsonPath)
+  scripts.prepare && execSync('npm pkg delete scripts.prepare')
 
   // 执行npm install
-  execSync("npm install");
+  execSync('npm install')
 
-  setPackageJsonSettings([{ "scripts.prepare": "husky install" }]);
+  setPackageJsonSettings([{ 'scripts.prepare': 'husky install' }])
   // 初始化仓库地址，防止pre-commit钩子关联失败
-  execSync("git init");
-  execSync("npm run prepare");
-};
+  execSync('git init')
+  execSync('npm run prepare')
+}
 
 /**
  * 检查指定package是否已安装在package.json的dependencies中
  * @param {string[]} packageNames 依赖列表, 例如 ['husky', 'lint-stage']
  * @returns {boolean[]} 依赖是否已安装列表
  */
-const checkInstalledPackage = (packageNames) => {
-  const packagePath = getPackageJsonPath();
-  const {
-    dependencies = {},
-    devDependencies = {},
-    optionalDependencies = {},
-  } = require(packagePath);
+const checkInstalledPackage = packageNames => {
+  const packagePath = getPackageJsonPath()
+  const { dependencies = {}, devDependencies = {}, optionalDependencies = {} } = require(packagePath)
   const dependenciesList = [
     ...Object.keys(dependencies),
     ...Object.keys(devDependencies),
-    ...Object.keys(optionalDependencies),
-  ];
-  return packageNames.map((item) => dependenciesList.includes(item));
-};
+    ...Object.keys(optionalDependencies)
+  ]
+  return packageNames.map(item => dependenciesList.includes(item))
+}
 
 /**
  * 根据依赖列表获取依赖的版本号
  */
-const getLatestLintDependencies = async (dependencies) => {
-  const dependenciesList = [];
+const getLatestLintDependencies = async dependencies => {
+  const dependenciesList = []
   const promiseList = dependencies.map(
-    (item) =>
-      new Promise((resolve) => {
+    item =>
+      new Promise(resolve => {
         exec(`npm view ${item} version`, (err, stdout) => {
           if (err) {
-            console.error(err);
+            console.error(err)
           }
-          resolve(stdout.toString().trim());
-        });
+          resolve(stdout.toString().trim())
+        })
       })
-  );
-  const latestVersions = await Promise.all(promiseList);
+  )
+  const latestVersions = await Promise.all(promiseList)
   dependencies.forEach((item, index) => {
-    dependenciesList.push(`${item}@^${latestVersions[index]}`);
-  });
+    dependenciesList.push(`${item}@^${latestVersions[index]}`)
+  })
 
-  return dependenciesList;
-};
+  return dependenciesList
+}
 
 /**
  * 添加dependencies到package.json文件的devDependencies中, 并排序
  * @param {string[]} dependencies 依赖列表, 例如 ['eslint@^8.40.0', 'prettier@^2.8.8']
  */
-const addLintDependencies = (dependencies) => {
-  const packagePath = getPackageJsonPath();
-  writePackageJson(packagePath, (packageJson) => {
-    const { devDependencies = {} } = packageJson;
-    dependencies.forEach((item) => {
-      const [name, version] = item.split("@^");
-      devDependencies[`${name}`] = `^${version}`;
-    });
-    packageJson.devDependencies = sortDependencies(devDependencies);
-    return packageJson;
-  });
-};
+const addLintDependencies = dependencies => {
+  const packagePath = getPackageJsonPath()
+  writePackageJson(packagePath, packageJson => {
+    const { devDependencies = {} } = packageJson
+    dependencies.forEach(item => {
+      const [name, version] = item.split('@^')
+      devDependencies[`${name}`] = `^${version}`
+    })
+    packageJson.devDependencies = sortDependencies(devDependencies)
+    return packageJson
+  })
+}
 
 /**
  * 排序dependencies
  */
-const sortDependencies = (dependencies) => {
-  const dependenciesList = Object.keys(dependencies);
-  const sortedDependencies = {};
-  dependenciesList.sort().forEach((item) => {
-    sortedDependencies[item] = dependencies[item];
-  });
-  return sortedDependencies;
-};
+const sortDependencies = dependencies => {
+  const dependenciesList = Object.keys(dependencies)
+  const sortedDependencies = {}
+  dependenciesList.sort().forEach(item => {
+    sortedDependencies[item] = dependencies[item]
+  })
+  return sortedDependencies
+}
 
 /**
  * 设置package.json的配置
  * @param {string|string[]} settings [{key: value}, {key: value}]
  */
-const setPackageJsonSettings = (settings) => {
+const setPackageJsonSettings = settings => {
   if (Array.isArray(settings)) {
-    let setting = "";
-    settings.forEach((item) => {
-      setting += `${Object.keys(item)[0]}="${Object.values(item)[0]}" `;
-    });
-    execSync(`npm pkg set ${setting.trim()}`);
+    let setting = ''
+    settings.forEach(item => {
+      setting += `${Object.keys(item)[0]}="${Object.values(item)[0]}" `
+    })
+    execSync(`npm pkg set ${setting.trim()}`)
   }
-};
+}
 
 /**
  * 批量生成lint配置文件到项目的根目录
  * @param {string}
  */
 const generateLintConfigs = () => {
-  const configPath = path.join(__dirname, "./../");
-  const lintTarget = ["eslint", "stylelint", "commitlint", "prettier"];
+  const lintTarget = ['eslint', 'stylelint', 'commitlint', 'prettier']
   // 获取当前项目名称
-  const _projectName = getProjectName();
-  const projectName = _projectName === 'ly-lint' ? '.' : _projectName;
-  const processDir = getProcessDir();
-  lintTarget.forEach((item) => {
-    const isCommitLint = item === "commitlint";
-    const fileName = isCommitLint ? "commitlint.config.js" : `.${item}rc.cjs`;
-    const lintConfigPath = path.join(processDir, fileName);
+  const projectName = getProjectName()
+  const processDir = getProcessDir()
+  lintTarget.forEach(item => {
+    const isCommitLint = item === 'commitlint'
+    const fileName = isCommitLint ? 'commitlint.config.cjs' : `.${item}rc.cjs`
+    const lintConfigPath = path.join(processDir, fileName)
 
     if (isCommitLint) {
-      const content = `module.exports = {\n  extends: [require.resolve('${projectName}/config/commitlint')],\n  rules: {}\n}
-      `;
-      writeFileSync(lintConfigPath, content);
-      return;
+      const content = `module.exports = {\n  extends: [require.resolve('${projectName}/config/commitlint')],\n  rules: {}\n}\n`
+      writeFileSync(lintConfigPath, content)
+      return
     } else {
       // 配置文件
-      const content = `const config = require('${projectName}/config/${item}')\n\nmodule.exports = config({})`;
-      writeFileSync(lintConfigPath, content);
+      const content = `const config = require('${projectName}/config/${item}')\n\nmodule.exports = config({})\n`
+      writeFileSync(lintConfigPath, content)
 
       // 忽略文件
-      const lintIgnorePath = path.join(processDir, `.${item}ignore`);
-      const ignoreContent = require(`${configPath}/config/${item}/ignore`);
+      const lintIgnorePath = path.join(processDir, `.${item}ignore`)
+      const ignoreContent = require(`./../config/${item}/ignore`)
 
-      writeFileSync(lintIgnorePath, ignoreContent);
+      writeFileSync(lintIgnorePath, ignoreContent)
     }
-  });
-};
+  })
+}
 
 /**
  * 获取当前项目名称
  * @returns {string} 项目名称
  */
 const getProjectName = () => {
-  const currentPackageJsonPath = getAbsolutePath("../package.json");
-  const { name } = require(currentPackageJsonPath);
-  return name;
-};
+  const currentPackageJsonPath = getAbsolutePath('../package.json')
+  const { name } = require(currentPackageJsonPath)
+  return name
+}
 
 /**
  * .vscode/settings.json文件
  * 包含了eslint、prettier、stylelint等配置
  */
 const generateVscodeSettings = () => {
-  const processDir = getProcessDir();
-  const settingsPath = path.join(processDir, ".vscode/settings.json");
+  const processDir = getProcessDir()
+  const settingsPath = path.join(processDir, '.vscode/settings.json')
   const settingsContent = {
-    "editor.formatOnSave": true,
-    "editor.codeActionsOnSave": {
-      "source.fixAll.eslint": true,
-      "source.fixAll.stylelint": true,
+    'editor.formatOnSave': true,
+    'editor.codeActionsOnSave': {
+      'source.fixAll.eslint': true,
+      'source.fixAll.stylelint': true
     },
-    "eslint.validate": ["javascript", "typescript", "vue"],
-    "stylelint.validate": ["css", "scss", "vue", "html", "postcss"],
-  };
-  writeFileSync(settingsPath, JSON.stringify(settingsContent, null, 2));
-};
+    'eslint.validate': ['javascript', 'typescript', 'vue'],
+    'stylelint.validate': ['css', 'scss', 'vue', 'html', 'postcss']
+  }
+  writeFileSync(settingsPath, JSON.stringify(settingsContent, null, 2))
+}
 
 /**
  * .vscode/extensions.json文件
  * 包含了校验需要依赖的插件配置
  */
 const generateVscodeExtensions = () => {
-  const processDir = getProcessDir();
-  const extensionsPath = path.join(processDir, ".vscode/extensions.json");
+  const processDir = getProcessDir()
+  const extensionsPath = path.join(processDir, '.vscode/extensions.json')
   const extensionsContent = {
-    recommendations: [
-      "Vue.volar",
-      "dbaeumer.vscode-eslint",
-      "esbenp.prettier-vscode",
-      "stylelint.vscode-stylelint",
-    ],
-  };
-  writeFileSync(extensionsPath, JSON.stringify(extensionsContent, null, 2));
-};
+    recommendations: ['Vue.volar', 'dbaeumer.vscode-eslint', 'esbenp.prettier-vscode', 'stylelint.vscode-stylelint']
+  }
+  writeFileSync(extensionsPath, JSON.stringify(extensionsContent, null, 2))
+}
 
 /**
  * 添加lint执行脚本到package.json文件的scripts中，以及 lint-staged配置
  */
 const addLintScripts = () => {
-  const packagePath = getPackageJsonPath();
-  writePackageJson(packagePath, (packageJson) => {
-    const { scripts = {}, "lint-staged": lintStaged = {} } = packageJson;
+  const packagePath = getPackageJsonPath()
+  writePackageJson(packagePath, packageJson => {
+    const { scripts = {} } = packageJson
+    // 删除相似功能脚本配置
+    Object.entries(scripts).map(([k, v]) => {
+      if (['lint', 'prettier', 'stylelint', 'style'].find(item => k.indexOf(item) > -1)) {
+        delete scripts[k]
+      }
+      if (['build', 'deploy'].find(item => k.indexOf(item) > -1)) {
+        scripts[k] = v.replace('vue-tsc --noEmit &&', '').replace('vue-tsc &&', '').trim()
+      }
+    })
+    scripts['lint:eslint'] =
+      'vue-tsc --noEmit && eslint . --ext .vue,.js,.jsx,.cjs,.ts,.tsx --fix --ignore-path .eslintignore'
+    scripts['lint:css'] = 'stylelint **/*.{vue,css,sass,scss} --fix'
+    const lintStaged = {
+      '*.{js,vue,ts}': 'npm run lint:eslint',
+      '*.{vue,css,scss,sass}': 'npm run lint:css'
+    }
+    scripts['prepare'] = 'husky install'
 
-    scripts["lint:eslint"] =
-      "vue-tsc --noEmit && eslint . --ext .vue,.js,.jsx,.cjs,.ts,.tsx --fix --ignore-path .eslintignore";
-    scripts["lint:css"] = "stylelint **/*.{vue,css,sass,scss} --fix";
-    lintStaged["*.{js,vue,ts}"] = "npm run lint:eslint";
-    lintStaged["*.{vue,css,scss,sass}"] = "npm run lint:css";
-    scripts["prepare"] = "husky install";
-
-    packageJson.scripts = scripts;
-    packageJson["lint-staged"] = lintStaged;
-    return packageJson;
-  });
-};
+    packageJson.scripts = scripts
+    packageJson['lint-staged'] = lintStaged
+    return packageJson
+  })
+}
 
 /**
  * 生成husky配置文件
  */
 const generateHuskyConfig = () => {
-  const processDir = getProcessDir();
-  const preCommitPath = path.join(processDir, ".husky/pre-commit");
-  const commitMsgPath = path.join(processDir, ".husky/commit-msg");
+  const processDir = getProcessDir()
+  const preCommitPath = path.join(processDir, '.husky/pre-commit')
+  const commitMsgPath = path.join(processDir, '.husky/commit-msg')
   // 创建 pre-commit 和 commit-msg 内容
   const preCommitContent = `#!/usr/bin/env sh
   . "$(dirname -- "$0")/_/husky.sh"
   
-  npx lint-staged`;
+  npx lint-staged`
   const commitMsgContent = `#!/usr/bin/env sh
   . "$(dirname -- "$0")/_/husky.sh"
   
-  npx --no-install commitlint --edit "$1"`;
+  npx --no-install commitlint --edit "$1"`
 
   // 内容覆盖
-  writeFileSync(preCommitPath, preCommitContent);
-  writeFileSync(commitMsgPath, commitMsgContent);
-};
+  writeFileSync(preCommitPath, preCommitContent)
+  writeFileSync(commitMsgPath, commitMsgContent)
+}
 
 module.exports = {
   checkNodeEnv,
@@ -370,5 +352,5 @@ module.exports = {
   generateVscodeSettings,
   generateVscodeExtensions,
   addLintScripts,
-  generateHuskyConfig,
-};
+  generateHuskyConfig
+}
